de0aa01ba554c23ce33fa8d224035e9ad6b09eef
nljacobson <89871547+nljacobson@users.noreply.github.com>
2023-04-28 11:24:17 -0400

    Created Repository

---
+++ data_model.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import sqlite3
+import csv
+import os
+
+
+# Import features from model
+def get_data(load_new = False) -> pd.DataFrame:
+    db = "chess_games.db"
+    con = sqlite3.connect(db)
+    cur = con.cursor()
+    if load_new:
+        # CREATE TABLE
+        create_table = '''CREATE TABLE IF NOT EXISTS game(id,
+                        INTEGER,
+                        player TEXT,
+                        player_name TEXT,
+                        url TEXT,
+                        white_Accuracy TEXT,
+                        black_Accuracy TEXT, 
+                        Event TEXT,
+                        Site TEXT,
+                        Date TEXT,
+                        White TEXT,
+                        Black TEXT,
+                        Result TEXT,
+                        BlackElo TEXT,
+                        ECO TEXT,
+                        ECOurl TEXT,
+                        EndDate TEXT,
+                        EndTime TEXT,
+                        Termination TEXT,
+                        TimeControl TEXT,
+                        WhiteElo TEXT,
+                        pgn TEXT,
+                        player_rating TEXT
+                    )'''
+        cur.execute(create_table)
+        # READ GAMES FROM CSV
+        file = open('Model/GM_games_dataset.csv',  encoding='utf-8')
+        contents = csv.reader(file)
+        # INSERT INTO SQL DATABASE
+        insert_records = '''INSERT INTO game (id,
+                        player,
+                        player_name,
+                        url,
+                        white_Accuracy,
+                        black_Accuracy,
+                        Event,
+                        Site,
+                        Date,
+                        White,
+                        Black,
+                        Result,
+                        BlackElo,
+                        ECO,
+                        ECOurl,
+                        EndDate,
+                        EndTime,
+                        Termination,
+                        TimeControl,
+                        WhiteElo,
+                        pgn,
+                        player_rating) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'''
+        cur.executemany(insert_records, contents)
+        con.commit()
+    # SELECT ALL VALUES
+    select_all = "SELECT White, Black, TimeControl, pgn FROM game"
+    df = pd.read_sql_query(select_all, con)
+    con.commit()
+    con.close()
+    return df
---
+++ main.py
@@ -0,0 +1,4 @@
+from data_model import get_data
+
+df = get_data()
+print(df['White'])
